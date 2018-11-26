09acdd15e4585ab724dbcb744c4994dad56f983e
chuanchuan <1072641997@qq.com>
2018-11-26 09:10:25 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,47 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import {getopenId,setopenId,removeopenId} from '@/utils/cookie.js'
 import {GetUrlParam} from '@/utils/urlparse.js'
-const _import = require('./_import_' + process.env.NODE_ENV)
+// const _import = require('./_import_' + process.env.NODE_ENV)
 
 Vue.use(Router)
+
+import HomeIndex from '../views/home/index'
+import HomeRecruitTab from '../views/home/recruitTab'
+import HomePostDetail from '../views/home/postDetail'
+import HomeEmploy from '../views/home/employ'
+import HomeEmployDetail from '../views/home/employDetail'
+import HomeUcenter from '../views/home/ucenter'
+import HomeEnterprise from '../views/home/enterprise'
+import HomeMycomm from '../views/home/mycomm'
+
+import UcenterPosition from '../views/ucenter/position'
+import UcenterSetup from '../views/ucenter/setup'
+import UcenterBoleEmployed from '../views/ucenter/bole/employed'
+import UcenterBoleRaters from '../views/ucenter/bole/raters'
+import UcenterObjectiveObjective from '../views/ucenter/objective/objective'
+import UcenterObjectiveEditIntent from '../views/ucenter/objective/editIntent'
+import UcenterResumeResume from '../views/ucenter/resume/resume'
+import UcenterResumeCertificate from '../views/ucenter/resume/certificate'
+import UcenterMycollectMycollect from '../views/ucenter/mycollect/mycollect'
+import UcenterMycollectCollect from '../views/ucenter/mycollect/collect'
+
+
+import LoginLogin from '../views/login/login'
+
+import EnterpriseSetup from '../views/enterprise/setup'
+import EnterpriseEnterpriseInfo from '../views/enterprise/enterpriseinfo'
+import EnterpriseJobmanageIndex from '../views/enterprise/jobmanage/index'
+import EnterpriseJobmanageJhistory from '../views/enterprise/jobmanage/jhistory'
+import EnterpriseJobmanageAlljob from '../views/enterprise/jobmanage/alljob'
+import EnterpriseJobmanageJobdetail from '../views/enterprise/jobmanage/jobdetail'
+import EnterpriseJobpostNewjob from '../views/enterprise/jobpost/newjob'
+import EnterpriseJobpostStationame from '../views/enterprise/jobpost/stationame'
+import EnterpriseJobpostStationtype from '../views/enterprise/jobpost/stationtype'
+import EnterpriseTabchangeArchives from '../views/enterprise/tabchange/archives'
+import EnterpriseTabchangeLibrary from '../views/enterprise/tabchange/library'
+import EnterpriseTabchangeReceived from '../views/enterprise/tabchange/received'
+
+
 
 const router = new Router({
     linkActiveClass: 'active',
@@ -16,46 +54,46 @@ const router = new Router({
         // 首页
         {
             path: '/home',
-            component: _import('home/index'),
+            component: HomeIndex,
             name: 'home',
             children: [
                 {
                     path: 'recruitIndex',
-                    component: _import('home/recruitTab'),
+                    component: HomeRecruitTab,
                     name: 'recruitIndex',
                     children: [
                         {
                             path: '/postDetail',
-                            component: _import('home/postDetail'),
+                            component: HomePostDetail,
                             name: 'postDetail',
                         }
                     ]
                 },
                 {
                     path: 'employ',
-                    component: _import('home/employ'),
+                    component: HomeEmploy,
                     name: 'employ',
                     children: [
                         {
                             path: '/employDetail',
-                            component: _import('home/employDetail'),
+                            component: HomeEmployDetail,
                             name: 'employDetail',
                         }
                     ]
                 },
                 {
                     path: 'ucenter',
-                    component: _import('home/ucenter'),
+                    component: HomeUcenter,
                     name: 'ucenter'
                 },
                 {
                     path: 'enterprise',
-                    component: _import('home/enterprise'),
+                    component: HomeEnterprise,
                     name: 'enterprise'
                 },
                 {
                     path: 'mycomm',
-                    component: _import('home/mycomm'),
+                    component: HomeMycomm,
                     name: 'mycomm'
                 },
 
@@ -64,57 +102,57 @@ const router = new Router({
         },
         {
             path: '/position',
-            component: _import('ucenter/position'),
+            component: UcenterPosition,
             name: 'position',
         },
         {
             path: '/employDetail',
-            component: _import('home/employDetail'),
+            component: UcenterEmployDetail,
             name: 'employDetail',
         },
         // 企业设置
         {
             path: '/esetup',
             name: 'esetup',
-            component:  _import('enterprise/setup')
+            component:  EnterpriseSetup
         },
         // ucenter设置
         {
             path: '/usetup',
             name: 'usetup',
-            component:  _import('ucenter/setup')
+            component:  UcenterSetup
         },
         {
             path: '/enterpriseInfo',
             name: 'enterpriseInfo',
-            component:  _import('enterprise/enterpriseInfo')
+            component:  EnterpriseEnterpriseInfo
         },
         // 职位管理
         {
             path: '/jobmanage',
             name: 'jobmanage',
             redirect: '/jobmanage/jhistory',
-            component:  _import('enterprise/jobmanage/index'),
+            component:  EnterpriseJobmanageIndex,
             children: [
                 {
                     path: 'jhistory',
                     name: 'jhistory',
-                    component: _import('enterprise/jobmanage/jhistory')
+                    component: EnterpriseJobmanageJhistory
                 },
                 {
                     path: 'records',
                     name: 'records',
-                    component: _import('enterprise/jobmanage/records')
+                    component: EnterpriseJobmanageRecords
                 },
                 {
                     path: 'alljob',
                     name: 'alljob',
-                    component: _import('enterprise/jobmanage/alljob')
+                    component: EnterpriseJobmanageAlljob
                 },
                 {
                     path: 'jobdetail',
                     name: 'jobdetail',
-                    component: _import('enterprise/jobmanage/jobdetail')
+                    component: EnterpriseJobmanageJobdetail
                 }
             ]
 
@@ -123,17 +161,17 @@ const router = new Router({
         {
             path: '/jobpost',
             name: 'jobpost',
-            component: _import('enterprise/jobpost/newjob'),
+            component: EnterpriseJobpostNewjob,
             children: [
                 {
                     path: 'stationame',
                     name: 'stationame',
-                    component: _import('enterprise/jobpost/stationame'),
+                    component: EnterpriseJobpostStationame
                 },
                 {
                     path: 'stationtype',
                     name: 'stationtype',
-                    component: _import('enterprise/jobpost/stationtype'),
+                    component: EnterpriseJobpostStationtype
                 }
             ]
         },
@@ -141,36 +179,36 @@ const router = new Router({
         {
             path: '/employed',
             name: 'employed',
-            component: _import('ucenter/bole/employed'),
+            component: UcenterBoleEmployed
         },
         // 评价
         {
             path: '/raters',
             name: 'raters',
-            component: _import('ucenter/bole/raters')
+            component: UcenterBoleRaters
         },
         // 求职意向
         {
             path: '/objective',
             name: 'objective',
-            component: _import('ucenter/objective/objective')
+            component: UcenterObjectiveObjective
         },
         {
             path: '/editIntent',
             name: 'editIntent',
-            component: _import('ucenter/objective/editIntent')
+            component: UcenterObjectiveEditIntent
         },
         // 登陆
         {
             path: '/login',
             name: 'login',
-            component: _import('login/login')
+            component: LoginLogin
         },
         // 我的简历
         {
             path:'/resume',
             name: 'resume',
-            component: _import('ucenter/resume/resume'),
+            component: UcenterResumeResume,
             // children: [
             //     {
             //         path: 'certificate',  //证书
@@ -183,34 +221,34 @@ const router = new Router({
 
             path: '/certificate',  //证书
             name: 'certificate',
-            component: _import('ucenter/resume/certificate')
+            component: UcenterResumeCertificate
 
         },
         // 我的收藏
         {
             path: '/mycollect',
             name: 'mycollect',
-            component: _import('ucenter/mycollect/mycollect')
+            component: UcenterMycollectMycollect
         },
         {
             path: '/collect',
             name: 'collect',
-            component: _import('ucenter/mycollect/collect')
+            component: UcenterMycollectCollect
         },
         {
             path: '/archives',
             name: 'archives',
-            component: _import('enterprise/tabchange/archives')
+            component: EnterpriseTabchangeArchives
         },
         {
             path: '/library',
             name: 'library',
-            component: _import('enterprise/tabchange/library')
+            component: EnterpriseTabchangeLibrary
         },
         {
             path: '/received',
             name: 'received',
-            component: _import('enterprise/tabchange/received')
+            component: EnterpriseTabchangeReceived
         },
 
     ]
